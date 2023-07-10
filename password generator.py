03488404604a7278e2cd8f8d23ba13ddf4757ad1
kartikj69 <85277352+kartikj69@users.noreply.github.com>
2023-07-11 04:51:25 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -25,4 +25,4 @@ password=""
 for char in password_list:
     password= password+char
 
-print(f"Your password is{password}")
+print(f"Your password is{''.join(random.sample(password,len(password)))}")
